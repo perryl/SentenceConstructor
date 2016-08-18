cbc0c40fe16fed09407c863680ae967df1a0d32c
Lauren Perry <lauren.perry@codethink.co.uk>
2016-08-18 16:32:20 +0100

    FragmentConstructor: rework codebase and add project json file

--- FragmentConstructor.cs
+++ FragmentConstructor.cs
@@ -1,46 +1,47 @@
 using System;
-namespace SentenceConstructor
-{
-    static void Main() {
-        int i = 0;
-        for (i < checkedFragments.Max(); i++) {
-            int j = i + 1;
-            for (j < checkedFragments.Max(); j++) {
-                // Run Enumerable.Intersect comparisons here
-                // Need some method of storing size of intersect along with value of i and j so we can return to them later for merging
-            }
-        }
-        string constructedSentence = 
-    }
+using System.Linq;
 
-    static string Array {
-        string fragments[] = new string[] {
+namespace ConsoleApplication
+{
+    public class FragmentConstructor
+    {
+        String fragments[] = {
             "Lorem ipsu", "psum dolor sit amet", "sit a", "it amet consectetur", "r adipiscing elit"
         }; // Five fragments comprising the first line of Lorem Ipsum
-        int maxStringLength = 1000; // Maximum allowed number of characters
-	List<string> list = new List<string>();
-        for (int i = 0; i < fragments.Max(); i++) {
-            if fragments[i].Length < maxStringLength && fragments[i].Length > 0 {
-                list.Add(fragments[i]);
+
+        public static void Main(string[] fragments)
+        {
+            var checkedFragments = CheckFragments(fragments);
+            int i = 0;
+            for (i < checkedFragments.Max(); i++) {
+                int j = i + 1;
+                for (j < checkedFragments.Max(); j++) {
+                    // Need some method of storing size of intersect along with value of i and j so we can return to
+                    // them later for merging
+                    // 3-value dictionary? Contains string a, string b, and string overlap (can get integer overlap
+                    // from str overlap.Length)
+                }
             }
         }
-        string[] checkedFragments = list.ToArray();
 
-        /* Double integer loop; set i = 0 and compare fragment[0] to each item in fragment[j] where j is originally set
-        at i + 1 but increments with each comparison, to a maximum of the size of the fragments array. once j reaches
-        increase i and rerun the process, starting once more with j = i + 1, as there is no need to repeat comparisons
-        (i.e. fragment[1] <> fragment[2] and fragment[2] <> fragment[1]) */
-    }
-    
-    static void Overlap {
-        // Check overlap
-        // Call merge function from here; keep functionality separate for readability
-    }
+        public static String[] CheckFragments(fragments)
+        {
+            // Perform sanity checking on sentence fragments (i.e. size < 1000 characters, characters valid)
+            return checkedFragments;
+        }
 
-    static void Merge {
-        if Overlap == fragments[i].Length() {
-            // Full string is subset of second string; can be considered merged
-            // Remove item from list
+        public static String Overlap()
+        {
+            // Run Enumerable.Intersect comparisons here
+            // Take two strings as argument, check if overlap between the two, return substring of overlapped chars
+            return overlap;
+        }
+
+        public static String Merge()
+        {
+            // Take two strings as argument, merge contents based on the overlap, return single merged string
+            // If overlap equal to full length of a string, consider string merged and return larger string
+            return mergedString;
         }
     }
 }
